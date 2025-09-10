743f358b0855227bb6d7c82636310285882d7a86
chandan <chandan@bitmaxgroup.com>
2025-09-10 12:49:44 +0530

    use wss

--- src/hooks/socket.js
+++ src/hooks/socket.js
@@ -7,10 +7,12 @@ export const connectSocket = () => {
   const token = localStorage.getItem("token");
 
   if (!socket || !socket.connected) {
-    socket = io("https://livechatcrm-byj4.onrender.com", {
+    socket = io("wss://https://livechatcrm-byj4.onrender.com", {
        path: "/socket.io",
       auth: { token },   // ✅ match backend socket.handshake.auth.token
       transports: ["websocket"],
+      createConnection: true,
+      secure: true,
     });
 
     socket.on("connect", () => {
